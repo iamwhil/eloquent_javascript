1705eb8478e9af3fe2c29996bfe0cecbce184229
Whil <ncsupiratecaptain@gmail.com>
2018-10-14 21:08:44 -0600

    Finishes ex3 for chapter 3.

--- Chapter 3/ex_3_A_List.js
+++ Chapter 3/ex_3_A_List.js
@@ -6,6 +6,35 @@ function arrayToList(array){
   }
 }
 
+function listToArray(list){
+  if (list.rest == null){
+    return [list.value];
+  } else {
+    return [list.value].concat(listToArray(list.rest));
+  }
+}
+
+function prepend(element, list){
+  return {value: element, rest: list};
+}
+
+function nth(list, n){
+  if (n == 0 ){
+    return (list.value);
+  } else if (list.rest == null){
+    return undefined;
+  } else {
+    return nth(list.rest, n - 1);
+  }
+}
+
 // Test Case
 array = [1, 2, 3];
-console.log(arrayToList(array));
+list = arrayToList(array);
+console.log("The list is ", list);
+array2 = listToArray(list);
+console.log("The array is", array2);
+list2 = prepend(0, list);
+console.log("Prepending 2 to list...", list2);
+console.log("Returning position 2", nth(list2, 2));
+console.log("Should be undefined (6)", nth(list2, 6));
