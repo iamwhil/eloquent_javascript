28cdcb4a7147258bf97f8ab6f700278825da0599
Whil Piavis <whil.piavis@redcanary.com>
2022-05-19 06:26:21 -0600

    Chapter 3 recursive function call

--- Chapter 2/ex_2_Recursion.js
+++ Chapter 2/ex_2_Recursion.js
@@ -1,9 +1,22 @@
-function isEven(num){
-  if (num == 1 || num == -1) { return false }
-  else if (num == 0) { return true }
-  else { return isEven(num - 2) }
+// Write a recursive function to find if n-2 is even or odd
+
+function evenOrOdd(n) { 
+  if(n == 0) {
+    return 'even'
+  } else if(n == 1) {
+      return 'odd'
+  } else {
+    return n > 0 ? evenOrOdd(n - 2) : evenOrOdd(n + 2)
+  }
 }
 
-//Test case
-console.log(isEven(50));
-console.log(isEven(75));
+console.log(evenOrOdd(0) == 'even')
+console.log(evenOrOdd(1) == 'odd')
+console.log(evenOrOdd(2) == 'even')
+console.log(evenOrOdd(10) == 'even')
+console.log(evenOrOdd(11) == 'odd')
+console.log(evenOrOdd(75) == 'odd')
+console.log(evenOrOdd(100) == 'even')
+console.log(evenOrOdd(-1) == 'odd')
+console.log(evenOrOdd(-2) == 'even')
+console.log(evenOrOdd(-20) == 'even')
