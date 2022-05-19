7b293748497792b2b5b75d81385cf025b61f1a0f
Whil Piavis <whil.piavis@redcanary.com>
2022-05-19 06:18:06 -0600

    chapter 3 exercise 1 - minimum defined 3 ways

--- Chapter 2/Ex_1_Minimum.js
+++ Chapter 2/Ex_1_Minimum.js
@@ -1,6 +1,15 @@
-//Attempt at making the minimum program as minimal as possible.
+// Find the minimum of 2 numbers
 
-min = (a, b) => (a <= b ? console.log(a) : console.log(b));
+const min = (a, b) => { a <= b ? console.log(a) : console.log(b) };
 
-// Simple Test Case
-min(8,2);
+let min2 = function(a, b) {
+  a <= b ? console.log(a) : console.log(b);
+};
+
+function min3(a, b) {
+  a <= b ? console.log(a) : console.log(b);
+};
+
+min(1, 2);
+min2(3, 4);
+min3(5, 6);
