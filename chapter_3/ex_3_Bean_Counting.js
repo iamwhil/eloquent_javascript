c2a2afdb7f77cf7861421f25296a87dd5b48ae0a
Whil Piavis <whil.piavis@redcanary.com>
2022-05-19 07:03:08 -0600

    chapter 3 exercise 3 character counting

--- chapter_3/ex_3_Bean_Counting.js
+++ chapter_3/ex_3_Bean_Counting.js
@@ -1,11 +1,13 @@
-function countChars(string, char){
-  char_count = 0;
-  for (i = 0; i < string.length; i++){
-    if (string[i] == char) { char_count++ };
-  };
-  console.log(`Number of ${char}'s: ${char_count}'`);
-};
+// Count the number of occurences of a character in a string.
 
-//Test case
-test_string = "Why hello there you handsome cat.";
-countChars(test_string, 'e');
+const countCharacter = (string, char) => {
+  count = 0
+  for(let c of string){
+    if(c == char) { count ++ }
+  }
+  console.log(`'${char}' occurs ${count} times in '${string}'`)
+}
+
+countCharacter('oh hello there', 't')
+countCharacter('oh hello there', 'o')
+countCharacter('oh hello there', 'v')
